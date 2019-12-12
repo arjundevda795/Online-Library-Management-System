5181fdf3e61229c195323a945e389b7eefb878f1
ARJUN DEVDA <47421104+arjundevda795@users.noreply.github.com>
2019-12-12 10:25:20 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2018 Product Details | Developed By :Group 13
+
+            </div>
+        </div>
+    </section>
